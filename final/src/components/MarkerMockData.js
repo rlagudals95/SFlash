67d882c86712d75ee093984d17a8f850b997a208
heo_minkyu <79818840+heo-mk@users.noreply.github.com>
2021-05-03 21:38:08 +0900

    Marker mock 데이터 추가

---
+++ final/src/components/MarkerMockData.js
@@ -0,0 +1,34 @@
+export const markerdata = [
+  { 
+    id: 1,
+    title: "우리집",
+    spotName: "우리집",
+    latitude: 35.57178114091584,
+    longitude: 128.76807144887422,
+    imgUrl: "https://i.postimg.cc/pLd7TqH5/Kakao-Talk-20210502-232358853.jpg",
+  },
+  {
+    id: 2,
+    title: "청도레일바이크",
+    spotName: "청도레일바이크",
+    latitude: 35.57610686087492,
+    longitude: 128.77388583964236,
+    imgUrl: "https://www.cheongdo.go.kr/open.content/tour/dynamic/photo/?v=26891|%EC%9E%85%EC%84%A0-%EA%B9%80%EC%A7%80%EC%9A%B0-%EC%B2%AD%EB%8D%94%EB%A0%88%EC%9D%BC%EB%B0%94%EC%9D%B4%ED%81%AC2.jpg",
+  },
+  {
+    id: 3,
+    title: "유천시장",
+    spotName: "유천시장",
+    latitude: 35.57044129370371,
+    longitude: 128.77186379625215,
+    imgUrl: "https://www.yeongnam.com/mnt/file/201810/20181016.010130736150001i1.jpg",
+  },
+  {
+    id: 4,
+    title: "유채꽃밭",
+    spotName: "유천유채꽃밭",
+    latitude: 35.566981995685396,
+    longitude: 128.76994573038047,
+    imgUrl: "https://i.postimg.cc/R0KTVHNn/Kakao-Talk-20210503-032434981.jpg",
+  },
+]
