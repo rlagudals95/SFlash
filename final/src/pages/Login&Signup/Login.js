3da4d663886b5d10e085e8c3c5e6d02dbc3c7a42
heo_minkyu <79818840+heo-mk@users.noreply.github.com>
2021-05-13 01:05:43 +0900

    소셜로그인 url 수정

--- final/src/pages/Login&Signup/Login.js
+++ final/src/pages/Login&Signup/Login.js
@@ -113,7 +113,7 @@ const Login = () => {
             color="#ffffff"
             onClick={() => {
               window.location.href =
-                "http://seungwook.shop/oauth2/authorize/naver?redirect_uri=http://localhost:3000/";
+                "http://13.125.97.117/oauth2/authorize/naver?redirect_uri=http://localhost:3000/";
               // onSocialLogin();
             }}
           >
@@ -123,7 +123,7 @@ const Login = () => {
             bg="#fee500"
             onClick={() => {
               window.location.href =
-                "http://seungwook.shop/oauth2/authorize/kakao?redirect_uri=http://localhost:3000/";
+                "http://13.125.97.117/oauth2/authorize/kakao?redirect_uri=http://localhost:3000/";
               // onSocialLogin();
             }}
           >
@@ -134,7 +134,7 @@ const Login = () => {
             color="#ffffff"
             onClick={() => {
               window.location.href =
-                "http://seungwook.shop/oauth2/authorize/google?redirect_uri=http://localhost:3000/";
+                "http://13.125.97.117/oauth2/authorize/google?redirect_uri=http://localhost:3000/";
               // onSocialLogin();
             }}
           >
