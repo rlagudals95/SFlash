f6c8cf3a582fc5447f23f4b28e833cac77341c8a
rlagudals95 <76252074+rlagudals95@users.noreply.github.com>
2021-05-13 18:43:19 +0900

    build

--- final/src/components/MyLocation.js
+++ final/src/components/MyLocation.js
@@ -4,16 +4,13 @@ import { useDispatch, useSelector } from "react-redux";
 import { history } from "../redux/configureStore";
 import sytled from "styled-components";
 
-const { kakao } = window; 
+const { kakao } = window;
 
 const MyLocation = (props) => {
-  const dispatch = useDispatch 
+  const dispatch = useDispatch;
 
   const [lati, setLati] = useState(0);
   const [longi, setLongi] = useState(0);
   const [address, setAddress] = useState("");
   console.log(address);
-
-  
-
-}
+};
--- final/src/shared/App.js
+++ final/src/shared/App.js
@@ -19,7 +19,7 @@ import Story from "../pages/Story";
 import NotFound from "../pages/NotFound";
 import SideNav from "../components/SideNav";
 import ScrollToTop from "./ScrollToTop"; //페이지 넘길때 스크롤 맨위로 초기화(무한 스크롤 때문에 필요함)
-import Cafe from "../pages/Category/Cafe";
+
 import About from "../pages/About";
 import Faq from "../pages/Faq";
 import QnaList from "../pages/QnaList";
@@ -82,7 +82,6 @@ function App() {
             <Route path="/qnawrite/:id" exact component={QnaWrite} />
             <Route component={NotFound} />
             {/* 밑에서 부턴 카테고리별 페이지 */}
-            <Route path="/cafe" exact component={Cafe} />
           </Switch>
         </ScrollToTop>
       </ConnectedRouter>
