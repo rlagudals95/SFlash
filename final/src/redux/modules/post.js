f75021a15feb4f48c58d8045bc1df17f395cac0d
rlagudals95 <76252074+rlagudals95@users.noreply.github.com>
2021-05-17 18:21:19 +0900

    조심조심

--- final/src/redux/modules/post.js
+++ final/src/redux/modules/post.js
@@ -9,6 +9,7 @@ import { config } from "../../shared/config";
 import { getCookie } from "../../shared/Cookie";
 import { push } from "react-router-redux";
 import { actionCreators as modalActions } from "./mapModal";
+import { add } from "lodash";
 
 const SET_POST = "SET_POST";
 const SET_MAP_POST = "SET_MAP_POST";
@@ -134,7 +135,7 @@ const addPostAPI = (post) => {
 
         // 게시물 올리면 바로 마커 뜨게 하기 api : 백엔드와 시험해보기
         // let one_post = res.data ? res.data.data?
-        // let one_post = res.data;
+        let one_post = res.data;
         // let one_marker_data = {
         //   id: one_post.boardId,
         //   category:  one_post.category,
@@ -145,8 +146,18 @@ const addPostAPI = (post) => {
         //   imgForOverlay: one_post.boardImgReponseDtoList[0].imgUrl,
         // };
         // dispatch(addMapPost(one_marker_data));
-        history.replace("/"); // 이부분 실행이 잘안되면 imgUrl인식을 못함 변수명 잘지켜주세요! : 민규 - 이건 데이터 변경없이 사이트만 변경해주는걸로 알고 있습니다
+        // history.replace("/"); // 이부분 실행이 잘안되면 imgUrl인식을 못함 변수명 잘지켜주세요! : 민규 - 이건 데이터 변경없이 사이트만 변경해주는걸로 알고 있습니다
         // window.location.replace("/"); // 민규 - 이 명령어는 데이터 변경이 반영되는 새로고침으로 알고 있어요. 게시물 업로드하고 반영된걸 바로 보려고 넣은 명령어에요.
+
+        // 커뮤니티 리덕스에 데이터 추가
+        let CommunityPost = {
+          id: one_post.boardId,
+          spotName: one_post.spotName,
+          img_url: one_post.boardImgReponseDtoList[0],
+          likeCnt: 0,
+          like: false,
+        };
+        dispatch(addPost(CommunityPost));
       })
       .catch((err) => {
         console.log(err);
