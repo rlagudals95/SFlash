384746d54b1496a79215ffb07c309f112a27dcba
heo_minkyu <79818840+heo-mk@users.noreply.github.com>
2021-05-18 00:07:08 +0900

    회의를느끼는중입니다

--- final/src/redux/modules/post.js
+++ final/src/redux/modules/post.js
@@ -147,16 +147,16 @@ const addPostAPI = (post) => {
         dispatch(addMapPost(one_marker_data));
         
         // 커뮤니티 리덕스에 데이터 추가
-        let CommunityPost = {
-          id: one_post.boardId,
-          spotName: one_post.spotName,
-          img_url: one_post.boardImgReponseDtoList[0],
-          likeCnt: 0,
-          like: false,
-        };
-        dispatch(addPost(CommunityPost));
+        // let CommunityPost = {
+        //   id: one_post.boardId,
+        //   spotName: one_post.spotName,
+        //   img_url: one_post.boardImgReponseDtoList[0],
+        //   likeCnt: 0,
+        //   like: false,
+        // };
+        // dispatch(addPost(CommunityPost));
       
-        history.replace("/"); 
+        // history.replace("/"); 
       })
       .catch((err) => {
         console.log(err);
@@ -264,21 +264,21 @@ const getMapPostAPI = () => {
         res.data.data.forEach((_post) => {
           let post = {
             id: _post.boardId, // 포스트 id
-            title: _post.title, // 포스트 title
-            content: _post.content, // 포스트 내용
             like: _post.liked,
-            likeCount: _post.likeCount,
             writerName: _post.writerName,
-            writerImgUrl: _post.writerImgUrl,
             latitude: _post.latitude,
             longitude: _post.longitude,
-            spotName: _post.spotName,
             spotName1: _post.spotName.split(" ")[0],
             spotName2: _post.spotName.split(" ").splice(1).join(" "),
             category: _post.category,
-            imgUrl: _post.boardImgReponseDtoList,
             imgForOverlay: _post.boardImgReponseDtoList[0].imgUrl,
-            comment: _post.boardDetailCommentDtoList,
+            // title: _post.title, // 포스트 title
+            // content: _post.content, // 포스트 내용
+            // likeCount: _post.likeCount,
+            // writerImgUrl: _post.writerImgUrl,        
+            // spotName: _post.spotName,
+            // imgUrl: _post.boardImgReponseDtoList,
+            // comment: _post.boardDetailCommentDtoList,
           };
           map_post_list.unshift(post);
         });
