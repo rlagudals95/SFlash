d972add178a9de8e54f8bcc1c17f7787772d2fbe
heo_minkyu <79818840+heo-mk@users.noreply.github.com>
2021-05-21 04:11:19 +0900

    현준님 url 추가

--- final/src/shared/config.js
+++ final/src/shared/config.js
@@ -1,10 +1,11 @@
 const config = {
-  api: "http://13.125.97.117",
+  api: "janghyeonjun.shop",
   jwt: localStorage.getItem("jwt"),
 };
 
 export { config };
 
+// 현준님: https://janghyeonjun.shop
 // 세정님: http://13.125.97.117
 // 승욱님: http://seungwook.shop
 
