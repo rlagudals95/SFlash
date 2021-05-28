4176b19f0d13c82e6aa26f786aa92831661ac61b
heo_minkyu <79818840+heo-mk@users.noreply.github.com>
2021-05-28 11:42:05 +0900

    env 설정

--- final/src/shared/config.js
+++ final/src/shared/config.js
@@ -1,12 +1,6 @@
 const config = {
-  // api: "https://janghyeonjun.shop",
   api: process.env.REACT_APP_API_URL,
   jwt: localStorage.getItem("jwt"),
 };
 
 export { config };
-
-// 현준님: https://janghyeonjun.shop
-// 세정님: http://13.125.97.117
-// 승욱님: http://seungwook.shop
-  
